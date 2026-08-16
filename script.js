619a47fdf4acfa46d123aad76fef99526e031fbc
click me <acut.bensalz@gmail.com>
2026-08-16 09:31:44 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+const nameInput = document.getElementById("name");
+const submitButton = document.getElementById("submit");
+const popup = document.getElementById("popup");
+const showText = document.getElementById("showText");
+const closeButton = document.getElementById("closeButton");
+const form = document.querySelector("form");
+const container = document.getElementById("container");
+const error = document.getElementById("error");
+const closeButton2 = document.getElementById("closeButton2");
+
+// Hide popup when the page loads
+popup.style.display = "none";
+error.style.display = "none";
+
+// Show popup
+function sayHello() {
+    const name = nameInput.value.trim();
+
+    if (name === "") {
+        nameInput.focus();
+        error.style.display = "flex";
+        popup.style.display = "none";
+        container.style.display = "none";
+        return;
+
+    }
+
+    showText.textContent = "WALA KANG JOWA " + name + "!";
+
+    popup.style.display = "flex";
+    container.style.display = "none";
+}
+
+// Submit button
+submitButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    sayHello();
+});
+
+// Press Enter
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    sayHello();
+});
+
+// Close popup
+closeButton.addEventListener("click", function() {
+    popup.style.display = "none";
+    container.style.display = "flex";
+    nameInput.value = "";
+});
+
+closeButton2.addEventListener("click", function() {
+    error.style.display = "none";
+    container.style.display = "flex";
+    nameInput.value = "";
+});
